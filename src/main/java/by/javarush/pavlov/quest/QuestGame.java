2a8a2aaac5ddab31c3535c52d61a0671f70f15ce
Dima Pavlov <dima.p96@mail.ru>
2023-10-01 13:48:04 +0300

    create class QuestGame and add two privet variables, added constructor, getters and setter for the next step

---
+++ src/main/java/by/javarush/pavlov/quest/QuestGame.java
@@ -0,0 +1,23 @@
+package by.javarush.pavlov.quest;
+
+public class QuestGame {
+    private String playerName;
+    private String currentStep;
+
+    public QuestGame(String playerName, String currentStep) {
+        this.playerName = playerName;
+        this.currentStep = currentStep;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setCurrentStep(String currentStep) {
+        this.currentStep = currentStep;
+    }
+
+    public String getCurrentStep() {
+        return currentStep;
+    }
+}
---
+++ src/main/java/by/javarush/pavlov/quest/QuestServlet.java
@@ -0,0 +1,17 @@
+package by.javarush.pavlov.quest;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+public class QuestServlet extends HttpServlet {
+
+    public void init() {
+
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+}
