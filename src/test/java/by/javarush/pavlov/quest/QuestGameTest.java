ead12b73a445cae78f0e1c7bd069bc6a3a91691e
Dima Pavlov <dima.p96@mail.ru>
2023-10-02 18:10:57 +0300

    Verifying that after creating a QuestGame instance, the player name and current step are initialized correctly

--- src/test/java/by/javarush/pavlov/quest/QuestGameTest.java
+++ src/test/java/by/javarush/pavlov/quest/QuestGameTest.java
@@ -1,7 +1,20 @@
 package by.javarush.pavlov.quest;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class QuestGameTest {
+    private QuestGame game;
 
+    @Before
+    public void setUp() {
+        game = new QuestGame("Player1");
+    }
+    @Test
+    public void testConstructor() {
+        assertEquals("Player1", game.getPlayerName());
+        assertEquals("start", game.getCurrentStep());
+    }
 }
